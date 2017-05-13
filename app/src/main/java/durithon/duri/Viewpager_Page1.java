2df83ff62ea246948bc56f53301c8c56775196c7
Hanul <nul010@naver.com>
2017-05-14 00:41:37 +0900

    목업 xml 및 액티비티

---
+++ app/src/main/java/durithon/duri/Viewpager_Page1.java
@@ -0,0 +1,26 @@
+package durithon.duri;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class Viewpager_Page1 extends android.support.v4.app.Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        LinearLayout linearLayout = (LinearLayout)inflater.inflate(R.layout.viewpager_child,container,false);
+
+        ImageView tutorial = (ImageView) linearLayout.findViewById(R.id.img_tutorial);
+//        tutorial.setImageResource(R.drawable.tu1);
+
+        return linearLayout;
+    }
+}
