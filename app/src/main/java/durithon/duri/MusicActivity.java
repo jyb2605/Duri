8ec6fbebc85364dc54307b6ca4131663b52ad98c
완빈 <a@ab.c>
2017-05-14 07:00:24 +0900

    record 최종확인 전

--- app/src/main/java/durithon/duri/MusicActivity.java
+++ app/src/main/java/durithon/duri/MusicActivity.java
@@ -10,11 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MusicActivity extends AppCompatActivity {
 
-
-    @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
-
     }
 }
