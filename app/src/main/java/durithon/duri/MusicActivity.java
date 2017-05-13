3a7cc1461fe3818e16cf920e1e560b29f0fd2ca9
JJH <isky4656@naver.com>
2017-05-14 02:51:26 +0900

    zzz

---
+++ app/src/main/java/durithon/duri/MusicActivity.java
@@ -0,0 +1,21 @@
+package durithon.duri;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by JJH on 2017-05-14.
+ */
+
+public class MusicActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_music);
+        
+
+    }
+}
